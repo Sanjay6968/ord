0f0c5842b934564ecefa84c84f59e3c4a601653d
vldot <lokesh.21bce8017@vitapstudent.ac.in>
2024-02-28 21:50:24 +0530

    nav menu icons, list

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -1,14 +1,15 @@
 // ** Icon imports
 import Login from 'mdi-material-ui/Login'
-import Table from 'mdi-material-ui/Table'
-import CubeOutline from 'mdi-material-ui/CubeOutline'
 import HomeOutline from 'mdi-material-ui/HomeOutline'
-import FormatLetterCase from 'mdi-material-ui/FormatLetterCase'
+
 import AccountCogOutline from 'mdi-material-ui/AccountCogOutline'
-import CreditCardOutline from 'mdi-material-ui/CreditCardOutline'
 import AccountPlusOutline from 'mdi-material-ui/AccountPlusOutline'
 import AlertCircleOutline from 'mdi-material-ui/AlertCircleOutline'
-import GoogleCirclesExtended from 'mdi-material-ui/GoogleCirclesExtended'
+import FormatListBulleted from 'mdi-material-ui/FormatListBulleted'
+import PackageVariant from 'mdi-material-ui/PackageVariant'
+import Printer3dNozzle from 'mdi-material-ui/Printer3dNozzle'
+import AccountMultiple from 'mdi-material-ui/AccountMultiple'
+import CurrencyRupee from 'mdi-material-ui/CurrencyRupee'
 
 // ** Type import
 import { VerticalNavItemsType } from 'src/@core/layouts/types'
@@ -24,6 +25,34 @@ const navigation = (): VerticalNavItemsType => {
       title: 'Account Settings',
       icon: AccountCogOutline,
       path: '/account-settings'
+    },
+    {
+      sectionTitle: 'Navigation Menu'
+    },
+    {
+      title: 'Orders',
+      icon: FormatListBulleted,
+      path: '/orders'
+    },
+    {
+      title: 'Inventory',
+      path: '/inventory',
+      icon: PackageVariant
+    },
+    {
+      title: 'Print Queue',
+      icon: Printer3dNozzle,
+      path: '/queue'
+    },
+    {
+      title: 'Customers',
+      icon: AccountMultiple,
+      path: '/customers'
+    },
+    {
+      icon: CurrencyRupee,
+      title: 'Financials',
+      path: '/financials'
     },
     {
       sectionTitle: 'Pages'
@@ -45,35 +74,35 @@ const navigation = (): VerticalNavItemsType => {
       icon: AlertCircleOutline,
       path: '/pages/error',
       openInNewTab: true
-    },
-    {
-      sectionTitle: 'User Interface'
-    },
-    {
-      title: 'Typography',
-      icon: FormatLetterCase,
-      path: '/typography'
-    },
-    {
-      title: 'Icons',
-      path: '/icons',
-      icon: GoogleCirclesExtended
-    },
-    {
-      title: 'Cards',
-      icon: CreditCardOutline,
-      path: '/cards'
-    },
-    {
-      title: 'Tables',
-      icon: Table,
-      path: '/tables'
-    },
-    {
-      icon: CubeOutline,
-      title: 'Form Layouts',
-      path: '/form-layouts'
     }
+    // {
+    //   sectionTitle: 'User Interface'
+    // },
+    // {
+    //   title: 'Typography',
+    //   icon: FormatLetterCase,
+    //   path: '/typography'
+    // },
+    // {
+    //   title: 'Icons',
+    //   path: '/icons',
+    //   icon: GoogleCirclesExtended
+    // },
+    // {
+    //   title: 'Cards',
+    //   icon: CreditCardOutline,
+    //   path: '/cards'
+    // },
+    // {
+    //   title: 'Tables',
+    //   icon: Table,
+    //   path: '/tables'
+    // },
+    // {
+    //   icon: CubeOutline,
+    //   title: 'Form Layouts',
+    //   path: '/form-layouts'
+    // }
   ]
 }
 
