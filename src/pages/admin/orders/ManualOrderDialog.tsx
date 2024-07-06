cc86dec50f8830b23b548aae8e09c08f41826bae
vldot <lokesh.21bce8017@vitapstudent.ac.in>
2024-07-06 16:43:53 +0530

    Use grid layout in ManualOrderDialog for better spacing

--- src/pages/admin/orders/ManualOrderDialog.tsx
+++ src/pages/admin/orders/ManualOrderDialog.tsx
@@ -1,5 +1,20 @@
 import React, { useState } from 'react';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, MenuItem, Select, FormControl, InputLabel, Box, SelectChangeEvent, Typography } from '@mui/material';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+  Box,
+  SelectChangeEvent,
+  Typography,
+  Grid
+} from '@mui/material';
 import { TechnologyDropdown } from 'src/layouts/components/TechnologyDropdown';
 import { MaterialDropdown, getMaterialOptions } from 'src/layouts/components/MaterialDropdown';
 import { ColorFinishDropdown, colorOptions } from 'src/layouts/components/ColorFinishDropdown';
@@ -58,225 +73,232 @@ const ManualOrderDialog: React.FC<ManualOrderDialogProps> = ({ open, onClose }) 
       <DialogContent>
         <Box mt={2}>
           <Typography variant="h6" gutterBottom>Customer Details</Typography>
-          <TextField
-            margin="dense"
-            name="customer"
-            label="Customer Name"
-            type="text"
-            fullWidth
-            value={orderData.customer}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="phone"
-            label="Phone Number"
-            type="text"
-            fullWidth
-            value={orderData.phone}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="price"
-            label="Price"
-            type="number"
-            fullWidth
-            value={orderData.price}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="status"
-            label="Status"
-            type="text"
-            fullWidth
-            value={orderData.status}
-            onChange={handleChange}
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                name="customer"
+                label="Customer Name"
+                type="text"
+                fullWidth
+                value={orderData.customer}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                name="email"
+                label="Email"
+                type="email"
+                fullWidth
+                value={orderData.email}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                name="phone"
+                label="Phone Number"
+                type="text"
+                fullWidth
+                value={orderData.phone}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                name="price"
+                label="Price"
+                type="number"
+                fullWidth
+                value={orderData.price}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                margin="dense"
+                name="status"
+                label="Status"
+                type="text"
+                fullWidth
+                value={orderData.status}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
         </Box>
 
         <Box mt={2}>
           <Typography variant="h6" gutterBottom>Customization</Typography>
-          <FormControl fullWidth margin="dense" variant="outlined">
-            <InputLabel>Technology</InputLabel>
-            <Select
-              value={orderData.technology}
-              onChange={handleSelectChange}
-              name="technology"
-              label="Technology"
-            >
-              {['FDM', 'SLA', 'SLS', 'DLP', 'MJF'].map(option => (
-                <MenuItem key={option} value={option}>
-                  {option}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl fullWidth margin="dense" variant="outlined">
-            <InputLabel>Material</InputLabel>
-            <Select
-              value={orderData.material}
-              onChange={handleSelectChange}
-              name="material"
-              label="Material"
-            >
-              {getMaterialOptions(orderData.technology).map(option => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl fullWidth margin="dense" variant="outlined">
-            <InputLabel>Layer Thickness</InputLabel>
-            <Select
-              value={orderData.layerThickness}
-              onChange={handleSelectChange}
-              name="layerThickness"
-              label="Layer Thickness"
-            >
-              {['Ultra Fine - 0.12mm', 'Fine - 0.16mm', 'Normal - 0.2mm', 'Draft - 0.3mm'].map(option => (
-                <MenuItem key={option} value={option}>
-                  {option}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl fullWidth margin="dense" variant="outlined">
-            <InputLabel>Printer</InputLabel>
-            <Select
-              value={orderData.printer}
-              onChange={handleSelectChange}
-              name="printer"
-              label="Printer"
-            >
-              {['STANDARD 220x200x220mm', 'MEDIUM 400x400x400mm', 'LARGE 600x600x600mm'].map(option => (
-                <MenuItem key={option} value={option}>
-                  {option}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <TextField
-            margin="dense"
-            name="infill"
-            label="Infill"
-            type="number"
-            fullWidth
-            value={orderData.infill}
-            onChange={handleChange}
-            InputProps={{ inputProps: { min: 0, max: 100, step: 10 } }}
-          />
-          <FormControl fullWidth margin="dense" variant="outlined">
-            <ColorFinishDropdown
-              options={colorOptions.getColorFinishOptions(orderData.technology, orderData.material)}
-              value={orderData.colorFinish}
-              onChange={(value) => setOrderData({ ...orderData, colorFinish: value })}
-            />
-          </FormControl>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth margin="dense" variant="outlined">
+                <InputLabel>Technology</InputLabel>
+                <Select
+                  value={orderData.technology}
+                  onChange={handleSelectChange}
+                  name="technology"
+                  label="Technology"
+                >
+                  {['FDM', 'SLA', 'SLS', 'DLP', 'MJF'].map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth margin="dense" variant="outlined">
+                <InputLabel>Material</InputLabel>
+                <Select
+                  value={orderData.material}
+                  onChange={handleSelectChange}
+                  name="material"
+                  label="Material"
+                >
+                  {getMaterialOptions(orderData.technology).map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth margin="dense" variant="outlined">
+                <InputLabel>Layer Thickness</InputLabel>
+                <Select
+                  value={orderData.layerThickness}
+                  onChange={handleSelectChange}
+                  name="layerThickness"
+                  label="Layer Thickness"
+                >
+                  {['Ultra Fine - 0.12mm', 'Fine - 0.16mm', 'Normal - 0.2mm', 'Draft - 0.3mm'].map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth margin="dense" variant="outlined">
+                <InputLabel>Printer</InputLabel>
+                <Select
+                  value={orderData.printer}
+                  onChange={handleSelectChange}
+                  name="printer"
+                  label="Printer"
+                >
+                  {['STANDARD 220x200x220mm', 'MEDIUM 400x400x400mm', 'LARGE 600x600x600mm'].map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                name="infill"
+                label="Infill"
+                type="number"
+                fullWidth
+                value={orderData.infill}
+                onChange={handleChange}
+                InputProps={{ inputProps: { min: 0, max: 100, step: 10 } }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth margin="dense" variant="outlined">
+                <ColorFinishDropdown
+                  options={colorOptions.getColorFinishOptions(orderData.technology, orderData.material)}
+                  value={orderData.colorFinish}
+                  onChange={(value) => setOrderData({ ...orderData, colorFinish: value })}
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
         </Box>
 
         <Box mt={2}>
           <Typography variant="h6" gutterBottom>Delivery Options</Typography>
-          <FormControl fullWidth margin="dense" variant="outlined">
-            <InputLabel>Delivery Type</InputLabel>
-            <Select
-              value={orderData.delivery_type}
-              onChange={handleSelectChange}
-              name="delivery_type"
-              label="Delivery Type"
-            >
-              {['Standard', 'Express'].map(option => (
-                <MenuItem key={option} value={option}>
-                  {option}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl fullWidth margin="dense" variant="outlined">
-            <InputLabel>Expert Assistance</InputLabel>
-            <Select
-              value={orderData.expert_assistance}
-              onChange={handleSelectChange}
-              name="expert_assistance"
-              label="Expert Assistance"
-            >
-              {['Required', 'Not Required'].map(option => (
-                <MenuItem key={option} value={option}>
-                  {option}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <TextField
-            margin="dense"
-            name="customer_name"
-            label="Customer Name"
-            type="text"
-            fullWidth
-            value={orderData.customer_name}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="email"
-            label="Email"
-            type="email"
-            fullWidth
-            value={orderData.email}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="phone"
-            label="Phone"
-            type="text"
-            fullWidth
-            value={orderData.phone}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="address"
-            label="Address"
-            type="text"
-            fullWidth
-            value={orderData.address}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="pincode"
-            label="Pincode"
-            type="text"
-            fullWidth
-            value={orderData.pincode}
-            onChange={handleChange}
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth margin="dense" variant="outlined">
+                <InputLabel>Delivery Type</InputLabel>
+                <Select
+                  value={orderData.delivery_type}
+                  onChange={handleSelectChange}
+                  name="delivery_type"
+                  label="Delivery Type"
+                >
+                  {['Standard', 'Express'].map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                name="address"
+                label="Address"
+                type="text"
+                fullWidth
+                value={orderData.address}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                name="pincode"
+                label="Pincode"
+                type="text"
+                fullWidth
+                value={orderData.pincode}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
         </Box>
 
         <Box mt={2}>
           <Typography variant="h6" gutterBottom>Checkout Options</Typography>
-          <TextField
-            margin="dense"
-            name="quantity"
-            label="Quantity"
-            type="number"
-            fullWidth
-            value={orderData.quantity}
-            onChange={handleChange}
-            InputProps={{ inputProps: { min: 1 } }}
-          />
-          <TextField
-            margin="dense"
-            name="gstNumber"
-            label="GST Number"
-            type="text"
-            fullWidth
-            value={orderData.gstNumber}
-            onChange={handleChange}
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                name="quantity"
+                label="Quantity"
+                type="number"
+                fullWidth
+                value={orderData.quantity}
+                onChange={handleChange}
+                InputProps={{ inputProps: { min: 1 } }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                name="gstNumber"
+                label="GST Number"
+                type="text"
+                fullWidth
+                value={orderData.gstNumber}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
         </Box>
       </DialogContent>
       <DialogActions>
